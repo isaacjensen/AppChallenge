7ad5918d76bb6d93559a8061aca40a8281d62ae6
roxy <roxy>
2016-10-16 14:38:03 -0700

    Camera + Saving settings preferances

--- app/src/main/java/com/example/craig/test/MainActivity.java
+++ app/src/main/java/com/example/craig/test/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.craig.test;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.media.Image;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,17 +14,113 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Spinner;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static ImageAdapter imageAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        imageAdapter = new ImageAdapter(this);
+        GridView grid = (GridView)(findViewById(R.id.imageGrid));
+        grid.setAdapter(imageAdapter);
+    Button camera = (Button)(findViewById(R.id.cameraButton));
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            GridView grid = (GridView)(findViewById(R.id.imageGrid));
+
+            imageAdapter.images.add(imageBitmap);
+            grid.refreshDrawableState();
+
+        }
+    }
+    public class ImageAdapter extends BaseAdapter {
+        private Context mContext;
+
+        // Keep all Images in array
+        public ArrayList<Bitmap> images = new ArrayList<Bitmap>();
 
 
+        // Constructor
+        public ImageAdapter(Context c){
+            mContext = c;
+        }
+
+        @Override
+        public int getCount() {
+            return images.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return images.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+            if (convertView == null) {
+                imageView = new ImageView(mContext);
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            }
+            else {
+                imageView = (ImageView) convertView;
+            }
+            imageView.setImageBitmap(images.get(position));
+            //imageView.setLayoutParams(new GridView.LayoutParams(70, 70));
+            return imageView;
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Spinner periodSpinner = (Spinner)(findViewById(R.id.periodSpinner));
+
+        final SharedPreferences sharedPref = this.getSharedPreferences("PeriodNames", Context.MODE_PRIVATE);
+        String p1 = sharedPref.getString("period1", "");
+        String p2 = sharedPref.getString("period2", "");
+        String p3 = sharedPref.getString("period3", "");
+        String p4 = sharedPref.getString("period4", "");
+        String p5 = sharedPref.getString("period5", "");
+        String p6 = sharedPref.getString("period6", "");
+
+        String[] items = new String[]{p1, p2, p3,p4,p5,p6};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        periodSpinner.setAdapter(adapter);
     }
 
     @Override
--- app/src/main/java/com/example/craig/test/SettingsActivity.java
+++ app/src/main/java/com/example/craig/test/SettingsActivity.java
@@ -1,7 +1,12 @@
 package com.example.craig.test;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -9,5 +14,41 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        final SharedPreferences sharedPref = this.getSharedPreferences("PeriodNames", Context.MODE_PRIVATE);
+
+
+
+        String p1 = sharedPref.getString("period1", "");
+        String p2 = sharedPref.getString("period2", "");
+        String p3 = sharedPref.getString("period3", "");
+        String p4 = sharedPref.getString("period4", "");
+        String p5 = sharedPref.getString("period5", "");
+        String p6 = sharedPref.getString("period6", "");
+
+        final EditText inputP1 = (EditText)(findViewById(R.id.editTextPeriod1));
+        final EditText inputP2 = (EditText)(findViewById(R.id.editTextPeriod2));
+        inputP1.setText(p1);
+        inputP2.setText(p2);
+
+        Button save = (Button)(findViewById(R.id.saveButton));
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //read values from UX
+                //save them
+
+                String pr1 = inputP1.getText().toString();
+                String pr2 = inputP2.getText().toString();
+
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString("period1", pr1);
+                editor.putString("period2", pr2);
+                editor.commit();
+
+
+            }
+        });
+
+
     }
 }
